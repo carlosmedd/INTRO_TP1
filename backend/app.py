fcc27b800aeda6820b379c5f72acbf22b72b95dc
carlosmeddev <medranocarlosjesus@gmail.com>
2024-06-29 01:31:48 -0300

    Detalles arreglados

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,6 @@ from models import db, User
 app = Flask(__name__)
 CORS(app)
 
-# Configuración de la base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://user_test:Yrrz34cwppuyk@localhost:5432/users_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
@@ -26,16 +25,7 @@ def login():
             "username": user.username,
             "password": user.password}), 200
     else:
-        return jsonify({"success": False, "message": "Credenciales inválidas"}), 401
-
-# @app.route('/current_user', methods=['GET', 'POST'])
-# def current_user():
-#     if request.method == 'POST'
-#     data = request.json
-#     user_id = data.get('id')
-
-#     user = User.query.filter_by(id=user_id).first()
-    
+        return jsonify({"success": False, "message": "Credenciales inválidas"}), 401 
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
